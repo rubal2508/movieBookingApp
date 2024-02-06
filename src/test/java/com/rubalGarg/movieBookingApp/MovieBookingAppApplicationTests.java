49489a2def4dbb35e54c393aa2ee4571a62233d9
Aman Garg <42492732+rubal2508@users.noreply.github.com>
2024-02-06 22:44:29 +0530

    add sql server database

--- src/test/java/com/rubalGarg/movieBookingApp/MovieBookingAppApplicationTests.java
+++ src/test/java/com/rubalGarg/movieBookingApp/MovieBookingAppApplicationTests.java
@@ -1,13 +1,12 @@
 package com.rubalGarg.movieBookingApp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovieBookingAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
